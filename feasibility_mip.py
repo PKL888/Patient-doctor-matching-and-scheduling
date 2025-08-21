c71106a9a10c3e482131c6a34915a9ae8883feaf
Tyler Pearn <tyler.pearn1@gmail.com>
2025-08-21 11:54:49 +1000

    Added constraint in mip that prevents doctors from being overbooked, untested :(

--- feasibility_mip.py
+++ feasibility_mip.py
@@ -7,19 +7,22 @@ J = range(4)
 I = range(10)
 
 disease_treat_times = data_gen.gen_diseases(K)
-doctor_treat_times = data_gen.gen_doctor_time_treat(J, K, disease_treat_times)
-doctor_can_treat = data_gen.gen_doctor_can_treat(doctor_treat_times, T)
+treat = data_gen.gen_doctor_time_treat(J, K, disease_treat_times)
+doctor_can_treat = data_gen.gen_doctor_can_treat(treat, T)
 doctor_disease_prefs = data_gen.gen_doctor_disease_preferences(doctor_can_treat)
-doctor_availability = data_gen.gen_doctor_times(T, K, J, doctor_treat_times, doctor_can_treat)
+doctor_availability = data_gen.gen_doctor_times(T, K, J, treat, doctor_can_treat)
 
-print(doctor_treat_times)
+print(treat)
 print(doctor_can_treat)
 print(doctor_disease_prefs)
 print(doctor_availability)
 
 patient_diseases = data_gen.gen_patient_diseases(I, K)
+
+I_k = [[i for i in I if patient_diseases[i] == k] for k in K]
+
 patient_doctor_prefs = data_gen.gen_patient_doctor_prefs(I, J, patient_diseases, doctor_can_treat)
-patient_times = data_gen.gen_patient_times(I, J, T, patient_diseases, doctor_can_treat, doctor_treat_times)
+patient_times = data_gen.gen_patient_times(I, J, T, patient_diseases, doctor_can_treat, treat)
         
 
 print(patient_diseases)
@@ -38,6 +41,11 @@ AllPatientsAreSeenAtMostOnce = \
 {i:
 m.addConstr(gp.quicksum(Y[i,j,t] for j in J for t in T) <= 1)
 for i in I}
+
+DoctorsAreNotOverbooked = \
+{(j,t):
+m.addConstr(gp.quicksum(Y[i,j,tt] for k in K for i in I_k[k] for tt in T[max(0, t-treat[j][k] + 1):t]) <= 1)
+for j in J for t in T}
 
 m.setObjective(gp.quicksum(Y[i,j,t] for i in I for j in J for t in T), gp.GRB.MAXIMIZE)
 
