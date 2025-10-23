a3bcf2ebf07434167d00a1db83688b4df3d27ab2
Peleg <36090606+PKL888@users.noreply.github.com>
2025-10-23 10:15:00 +1000

    Create pseudo code for main file

---
+++ src/main.py
@@ -0,0 +1,8 @@
+# import all functions
+
+# specify problem size, number of seeds --> generate data
+
+# choose model(s) --> run model(s)
+# (check whether a data or output file already exists)
+
+# create summary tables or comparison plots
